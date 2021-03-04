b1f70f016a3cede475c9b408b1930a5e70ca190c
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-04 15:00:51 +0000

    added homepage components

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,18 @@ import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 import PageHero from './PageHero';
+import Hero from './Hero';
+import Services from './Services';
+import Contact from './Contact';
+import FeaturedProducts from './FeaturedProducts';
 
-export { Navbar, Sidebar, Footer, PageHero };
+export {
+  Navbar,
+  Sidebar,
+  Footer,
+  PageHero,
+  Hero,
+  Services,
+  Contact,
+  FeaturedProducts,
+};
